83754960e9b7759fbd7cb2fc1d0f7e97551df39f
defy-ing <heather9705@naver.com>
2025-02-21 10:53:43 +0900

    LikeLion3

---
+++ study3/study3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //캐릭터
+            //hp : 100
+            //att : 56.7
+            //캐릭터이름 : junseo
+            //등급 : S
+
+            int hp = 100;
+            double att = 56.7;
+            char grade = 'S';
+            string name = "Junseo";
+
+            Console.WriteLine("캐릭터");
+            Console.WriteLine("공격력 : "+att);
+            Console.WriteLine("체력 : "+hp);
+            Console.WriteLine("등급 : "+grade);
+            Console.WriteLine("캐릭터이름 : "+name);
+
+
+
+
+
+
+        }
+    }
+}
