8c4db25a7856eb4cd5161e233299ca6fc3e29e89
defy-ing <heather9705@naver.com>
2025-02-21 10:23:56 +0900

    LikeLion2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age; //정수형 변수
+            age = 25; //변수에 값 저장
+      
+
+            Console.WriteLine(age); //변수에 저장된 값을 출력 25
+
+            //출력은 컨트롤 f5
+            //컨트롤+s
+        }
+    }
+}
